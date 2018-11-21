6a3a6490a5a60dc8a11a63d5da757fab77781291
manuel taveras <taveras78@gmail.com>
2018-11-21 14:00:26 -0400

    done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Welcome from './components/Welcome';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
