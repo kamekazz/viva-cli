135568fc1a19cd5d395e839c359a5270a21d606b
kamekazz <taveras78@gmail.com>
2018-11-26 03:26:49 -0800

    si

---
+++ src/confing.js
@@ -0,0 +1,11 @@
+const confing = {
+    API_KEY :'AIzaSyD0JpBJqOZVZIg-1sWtNwVE7KHSwVxtSj8',
+     apiUrl : 'https://api-viva.herokuapp.com'
+}
+
+
+// const confing = {
+//     API_KEY :'AIzaSyD0JpBJqOZVZIg-1sWtNwVE7KHSwVxtSj8',
+//      apiUrl : 'http://localhost:3090'
+// }
+export default confing
